461832b822412de52315fe1587fcad31602b7d5e
mhanhur <st4yzwd@gmail.com>
2026-04-04 14:33:00 +0300

    refactoring

--- js-operators/operators.js
+++ js-operators/operators.js
@@ -1,16 +1,17 @@
 // Task 1
-let lMeters = 5;
-let wWidth= 7;
-let hHeight = 2.5;
-let sqr = 2 * (lMeters + wWidth) * hHeight;
-let can = 4*4;
-
-console.log(Math.round(sqr / can));
+const canCalculationFunc = (lengthMeters, widthMeters, heightMeters) => {
+    let sqr = 2 * (lengthMeters + widthMeters) * heightMeters
+    let can = 4*4
+    console.log(Math.round(sqr / can))
+}
+canCalculationFunc(5,7,2.5)
 
 // Task 2
-let luckyTicket = 385916
-let digits = luckyTicket.toString().split('').map(Number)
-let firstPartOfTicket = digits.slice(0,3).reduce((a, b) => a + b, 0)
-let secondPartOfTicket = digits.slice(3,6).reduce((a, b) => a + b, 0)
+const luckyTicketFunc = (luckyTicket, firstPartOfTicket, secondPartOfTicket, digits) => {
+    digits = luckyTicket.toString().split('').map(Number)
+    firstPartOfTicket = digits.slice(0,3).reduce((a, b) => a + b, 0)
+    secondPartOfTicket = digits.slice(3,6).reduce((a, b) => a + b, 0)
+    console.log(firstPartOfTicket === secondPartOfTicket)
+}
+luckyTicketFunc(385916)
 
-console.log(firstPartOfTicket === secondPartOfTicket)
